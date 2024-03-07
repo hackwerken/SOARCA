af1aeeb065db9047e036b259bbc476eec8c2a5d9
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-03-07 14:40:29 +0100

    http capability uses http wrapper

--- internal/capability/http/http.go
+++ internal/capability/http/http.go
@@ -1,16 +1,11 @@
 package http
 
 import (
-	"bytes"
-	"encoding/base64"
-	"errors"
-	"io"
-	"net/http"
 	"reflect"
 	"soarca/logger"
 	"soarca/models/cacao"
 	"soarca/models/execution"
-	"strings"
+	"soarca/utils/http"
 )
 
 // Receive HTTP API command data from decomposer/executer
@@ -30,9 +25,6 @@ func init() {
 	log = logger.Logger(component, logger.Info, "", logger.Json)
 }
 
-// What to do if there is no agent or target?
-// And maybe no auth info either?
-
 func (httpCapability *HttpCapability) Execute(
 	metadata execution.Metadata,
 	command cacao.Command,
@@ -40,125 +32,21 @@ func (httpCapability *HttpCapability) Execute(
 	target cacao.AgentTarget,
 	variables cacao.VariableMap) (cacao.VariableMap, error) {
 
-	// Get request data and handle errors
-	method, url, errmethod := ObtainHttpMethodAndUrlFromCommand(command)
-	if errmethod != nil {
-		log.Error(errmethod)
-		return cacao.VariableMap{}, errmethod
-	}
-	content_data, errcontent := ObtainHttpRequestContentDataFromCommand(command)
-	if errcontent != nil {
-		log.Error(errcontent)
-		return variables, errcontent
+	soarca_http_request := new(http.HttpRequest)
+	soarca_http_options := http.HttpOptions{
+		Target:  &target,
+		Command: &command,
+		Auth:    &authentication,
 	}
 
-	// Setup request
-	request, err := http.NewRequest(method, url, bytes.NewBuffer(content_data))
-	if err != nil {
-		log.Error(err)
-		return cacao.VariableMap{}, err
-	}
-
-	for key, httpCapability := range command.Headers {
-		request.Header.Add(key, httpCapability)
-	}
-	if target.ID != "" {
-		if err := verifyAuthInfoMatchesAgentTarget(&target, &authentication); err != nil {
-			log.Error(err)
-			return cacao.VariableMap{}, err
-		}
-
-		if err := setupAuthHeaders(request, &authentication); err != nil {
-			log.Error(err)
-			return cacao.VariableMap{}, err
-		}
-	}
-
-	// Perform request
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		log.Error(err)
-		return cacao.VariableMap{}, err
-	}
-	defer response.Body.Close()
-
-	responseBytes, err := io.ReadAll(response.Body)
+	responseBytes, err := soarca_http_request.Request(soarca_http_options)
 	if err != nil {
 		log.Error(err)
 		return cacao.VariableMap{}, err
 	}
 	respString := string(responseBytes)
-	sc := response.StatusCode
-	if sc < 200 || sc > 299 {
-		return cacao.VariableMap{}, errors.New(respString)
-	}
 
 	return cacao.VariableMap{
 		"__soarca_http_result__": {Name: "result", Value: respString}}, nil
 
-}
-
-func ObtainHttpMethodAndUrlFromCommand(
-	command cacao.Command) (string, string, error) {
-	parts := strings.Split(command.Command, " ")
-	if len(parts) != 2 {
-		return "", "", errors.New("method or url missing from command")
-	}
-	method := parts[0]
-	url := parts[1]
-	return method, url, nil
-}
-
-func verifyAuthInfoMatchesAgentTarget(
-	target *cacao.AgentTarget, authInfo *cacao.AuthenticationInformation) error {
-	if !(target.AuthInfoIdentifier == authInfo.ID) {
-		return errors.New("target auth info id does not match auth info object's")
-	}
-	return nil
-}
-
-func setupAuthHeaders(request *http.Request, authInfo *cacao.AuthenticationInformation) error {
-
-	authInfoType := authInfo.Type
-	switch authInfoType {
-	case cacao.AuthInfoHTTPBasicType:
-		request.SetBasicAuth(authInfo.Username, authInfo.Password)
-	case cacao.AuthInfoOAuth2Type:
-		// TODO: verify correctness
-		// (https://datatracker.ietf.org/doc/html/rfc6750#section-2.1)
-		bearer := "Bearer " + authInfo.Token
-		request.Header.Add("Authorization", bearer)
-	case "":
-		// It means that AuthN information is not set
-		return nil
-	default:
-		return errors.New("unsupported authentication type: " + authInfoType)
-	}
-	return nil
-}
-
-func ObtainHttpRequestContentDataFromCommand(
-	command cacao.Command) ([]byte, error) {
-	// Reads if either command or command_b64 are populated, and
-	// Returns a byte slice from either
-	content := command.Content
-	contentB64 := command.ContentB64
-
-	var nil_content []byte
-
-	if content == "" && contentB64 == "" {
-		return nil_content, nil
-	}
-
-	if content != "" && contentB64 != "" {
-		log.Warn("both content and content_b64 are populated. using content.")
-		return []byte(content), nil
-	}
-
-	if content != "" {
-		return []byte(content), nil
-	}
-
-	return base64.StdEncoding.DecodeString(contentB64)
 }
